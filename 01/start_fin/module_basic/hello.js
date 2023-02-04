deee32726b4043259f71201c70df4764dbd1a159
Kevin You <eilgwon88@gmail.com>
2023-02-04 10:20:23 +0900

    feat: code

---
+++ 01/start_fin/module_basic/hello.js
@@ -0,0 +1,10 @@
+// export default function hello1() {
+// 	console.log('Hello 1!');
+// }
+export function hello1() {
+	console.log('Hello 1!');
+}
+
+export function hello2() {
+	console.log('Hello 2!');
+}
---
+++ 01/start_fin/module_basic/main.js
@@ -0,0 +1,8 @@
+// import { hello1, hello2 } from './hello.js';
+import * as hell from './hello.js';
+// import hello1 from './hello.js';
+
+
+hell.hello1();
+hell.hello2();
+// hello1();
