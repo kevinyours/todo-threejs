0adc782586824ee5b09b02b9af5f08ecdd1061f1
Kevin You <eilgwon88@gmail.com>
2023-02-14 20:06:33 +0900

    feat: code

---
+++ controls/src/main.js
@@ -0,0 +1,9 @@
+// import example from './ex01';
+// import example from "./ex02";
+// import example from "./ex03";
+// import example from "./ex04";
+// import example from "./ex05";
+// import example from "./ex06";
+import example from "./ex07";
+
+example();
