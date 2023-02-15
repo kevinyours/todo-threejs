9e99ab7d1fad2dc141c2e8745aa293d529b01687
Kevin You <eilgwon88@gmail.com>
2023-02-16 07:58:56 +0900

    feat: code

---
+++ material_fin/src/main.js
@@ -0,0 +1,19 @@
+import example from './ex01';
+// import example from './ex02';
+// import example from './ex03';
+// import example from './ex04';
+// import example from './ex05';
+// import example from './ex06';
+// import example from './ex07';
+// import example from './ex08';
+// import example from './ex09';
+// import example from './ex10';
+// import example from './ex11';
+// import example from './ex12';
+// import example from './ex13';
+// import example from './ex14';
+// import example from './ex15';
+// import example from './ex16';
+// import example from './ex17';
+
+example();
